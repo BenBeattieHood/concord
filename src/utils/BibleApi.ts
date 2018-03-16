90b93f908db8d10d216daf6a1790b4946b39b704
BenBeattieHood <benb@pageuppeople.com>
2018-03-16 16:57:15 +1100

    improving api shapes

--- src/utils/BibleApi.ts
+++ src/utils/BibleApi.ts
@@ -1,8 +1,25 @@
+function fetchJson<TResult>(uri:string) {
+    return (
+        fetch(uri)
+        .then(x => x.json())
+        .then((x:string) => JSON.parse(x) as TResult)
+    );
+}
+
+interface GetVersesResponse {
+    response: {
+        meta: {
+
+        },
+        verses: {
+            
+        }
+    }
+}
+
 export const GetCitations = (args:{book:string, chapter:number, fromVerse:number, toVerse:number}):Promise<string> => 
-    fetch(`https://bibles.org/v2/chapters/eng-KJVA:1Cor.2/verses.js?start=5&end=6`)
-    .then(x => x.json)
-    .then((x:string) => JSON.parse<{asd:string}>(x))
+    fetchJson<GetVersesResponse>(`https://bibles.org/v2/chapters/eng-KJVA:1Cor.2/verses.js?start=5&end=6`)
     .then(response => {
-        return ""
+        
     });
     //http://bibles.org/pages/api/documentation/verses
